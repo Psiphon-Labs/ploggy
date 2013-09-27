2bbf4c14658a3750c606d5d4f12f1d3230b40640
Rod Hynes <rod-hynes@users.noreply.github.com>
2013-09-27 14:27:03 -0400

    custom field naming for POJO --> JSON

--- src/ca/psiphon/ploggy/Json.java
+++ src/ca/psiphon/ploggy/Json.java
@@ -19,17 +19,21 @@
 
 package ca.psiphon.ploggy;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import com.google.gson.FieldNamingStrategy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 public class Json {
 
-    private static final Gson mSerializer = new Gson();
+    private static final Gson mSerializer =
+            new GsonBuilder().setFieldNamingStrategy(new CustomFieldNamingStrategy()).create(); 
     
     public static String toJson(Object object) {
     	return mSerializer.toJson(object);
@@ -50,5 +54,15 @@ public class Json {
         } catch (JsonSyntaxException e) {
             throw new Utils.ApplicationError(e);
         }
+    }
+
+    private static class CustomFieldNamingStrategy implements FieldNamingStrategy {
+
+        @Override
+        public String translateName(Field field) {
+            // TODO: ... mFieldName --> fieldName
+            return Character.toLowerCase(field.getName().charAt(1)) + field.getName().substring(2);
+        }
+        
     }
 }
