0b459ec11ecf5bcfb18463bee91492440d3f773a
Adam Pritchard <pritchard.adam@gmail.com>
2013-11-15 15:57:44 -0500

    Fixed TabListener code. Bad code was a result of bad Android documentation: https://code.google.com/p/android/issues/detail?id=62354

--- AndroidApp/src/ca/psiphon/ploggy/ActivityMain.java
+++ AndroidApp/src/ca/psiphon/ploggy/ActivityMain.java
@@ -6,12 +6,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -19,12 +19,12 @@
 
 package ca.psiphon.ploggy;
 
-import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -32,7 +32,7 @@ import android.view.WindowManager;
 
 /**
  * The main UI screen.
- * 
+ *
  * This activity displays a list of friends, along with a summary of their status.
  * Users can tab between the friend list and a list of event logs. The Action Bar
  * menu is populated with the main app actions.
@@ -67,7 +67,7 @@ public class ActivityMain extends ActivitySendIdentityByNfc {
                             new TabListener<FragmentFriendList>(
                                     this,
                                     "fragment_friend_list",
-                                    FragmentFriendList.class)));        
+                                    FragmentFriendList.class)));
         actionBar.addTab(
                 actionBar.newTab()
                     .setText(R.string.title_recent_activity_fragment)
@@ -76,7 +76,7 @@ public class ActivityMain extends ActivitySendIdentityByNfc {
                                     this,
                                     "fragment_recent_activity",
                                     FragmentRecentActivity.class)));
-                
+
         if (savedInstanceState != null) {
             actionBar.setSelectedNavigationItem(savedInstanceState.getInt("currentTab", 0));
         }
@@ -87,7 +87,7 @@ public class ActivityMain extends ActivitySendIdentityByNfc {
         super.onSaveInstanceState(outState);
         outState.putInt("currentTab", getActionBar().getSelectedNavigationIndex());
     }
-    
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -96,7 +96,7 @@ public class ActivityMain extends ActivitySendIdentityByNfc {
         // Don't show the keyboard until edit selected
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -133,35 +133,56 @@ public class ActivityMain extends ActivitySendIdentityByNfc {
             return super.onOptionsItemSelected(item);
         }
     }
-    
-    // Adapted from: http://developer.android.com/guide/topics/ui/actionbar.html#Tabs
+
+    // Adapted from: http://developer.android.com/reference/android/app/ActionBar.html#newTab%28%29
     private static class TabListener<T extends Fragment> implements ActionBar.TabListener {
         private Fragment mFragment;
         private final Activity mActivity;
         private final String mTag;
         private final Class<T> mClass;
+        private final Bundle mArgs;
+
 
         public TabListener(Activity activity, String tag, Class<T> clz) {
+            this(activity, tag, clz, null);
+
+        }
+
+        public TabListener(Activity activity, String tag, Class<T> clz, Bundle args) {
             mActivity = activity;
             mTag = tag;
             mClass = clz;
+            mArgs = args;
+
+            // Check to see if we already have a fragment for this tab, probably
+            // from a previously saved state.  If so, deactivate it, because our
+            // initial state is that a tab isn't shown.
+            mFragment = mActivity.getFragmentManager().findFragmentByTag(mTag);
+            if (mFragment != null && !mFragment.isDetached()) {
+                FragmentTransaction ft = mActivity.getFragmentManager().beginTransaction();
+                ft.detach(mFragment);
+                ft.commit();
+            }
         }
 
+        @Override
         public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
             if (mFragment == null) {
-                mFragment = Fragment.instantiate(mActivity, mClass.getName());
+                mFragment = Fragment.instantiate(mActivity, mClass.getName(), mArgs);
                 ft.add(android.R.id.content, mFragment, mTag);
             } else {
                 ft.attach(mFragment);
             }
         }
 
+        @Override
         public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
             if (mFragment != null) {
                 ft.detach(mFragment);
             }
         }
 
+        @Override
         public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
         }
     }
