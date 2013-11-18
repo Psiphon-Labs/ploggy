404da750f5ff140359a382c8f58079f934ab3f55
Rod Hynes <rod-hynes@users.noreply.github.com>
2013-11-18 12:44:39 -0500

    Start to use version number for prototype; minor fixes

--- AndroidApp/src/ca/psiphon/ploggy/Protocol.java
+++ AndroidApp/src/ca/psiphon/ploggy/Protocol.java
@@ -34,8 +34,8 @@ public class Protocol {
 
     public static final String PULL_STATUS_REQUEST_PATH = "/pullStatus";
 
+    // Limits in place for prototype, which is currently sending all messages in one blob
     public static int MAX_MESSAGE_LENGTH = 100;
-    
     public static int MAX_MESSAGE_COUNT = 50;
 
     public static boolean isValidNickname(String nickname) {
